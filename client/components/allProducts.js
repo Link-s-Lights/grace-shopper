c7203e71317343f20ddd8c5ae853982f1791deed
Stone Fuglaar <fuglaarusa@gmail.com>
2021-03-03 13:30:22 -0800

    Feature: single-product-view

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {setProducts} from '../store/products'
+import {Link} from 'react-router-dom'
 
 export class AllProducts extends React.Component {
   async componentDidMount() {
     await this.props.getMyProducts()
   }
   render() {
-    console.log(this.props.products)
     const productsArray = this.props.products
     if (productsArray) {
       return (
@@ -17,7 +17,9 @@ export class AllProducts extends React.Component {
             {productsArray.map(product => {
               return (
                 <div key={product.id}>
-                  <h1>{product.name}</h1>
+                  <Link to={`/products/${product.id}`}>
+                    <h1>{product.name}</h1>
+                  </Link>
                   <p>{product.description}</p>
                   <h2>${product.price}</h2>
                   <h2>Stock: {product.stock}</h2>
