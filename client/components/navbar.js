76c9f231d92215d87a84df715a93dd323ffe3696
Stone Fuglaar <fuglaarusa@gmail.com>
2021-03-03 15:05:52 -0800

    fixed navbar React routes all-products display

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -13,17 +13,18 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
-          <Link to="/products">All Products</Link>
+          {/* <Link to="/products">All Products</Link> */}
           <a href="#" onClick={handleClick}>
             Logout
           </a>
-          {/* <Route exact path="/products" component={allProducts} /> */}
+          <Link to="/products">All Products</Link>
         </div>
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
+          <Link to="/products">All Products</Link>
           {/* MAYBE ADD SWITCH HERE */}
         </div>
       )}
