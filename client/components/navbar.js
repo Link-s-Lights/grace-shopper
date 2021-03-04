a752c76b4af855fabdb976dfebea8ed9145c4e8d
Stone Fuglaar <fuglaarusa@gmail.com>
2021-03-04 14:26:36 -0800

    homescreen link to all products after login

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -50,7 +50,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
-          <Link to="/products">All Products</Link>
+          {/* <Link to="/products">All Products</Link> */}
           <Link to="/cart" className="cart-button btn btn-info btn-lg">
             <span className="glyphicon glyphicon-shopping-cart" /> Shopping Cart
           </Link>
--- client/routes.js
+++ client/routes.js
@@ -34,7 +34,7 @@ class Routes extends Component {
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
+        <Route component={AllProducts} />
       </Switch>
     )
   }
