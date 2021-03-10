39a131f2d79e254259ec73b6d99675e9e57dd965
Mark Eggensperger <markeggensperger@gmail.com>
2021-03-09 20:01:59 -0500

    fixed lowercase

--- client/components/index.js
+++ client/components/index.js
@@ -6,5 +6,5 @@
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
 export {Login, Signup} from './auth-form'
-export {default as Cart} from './Cart'
+export {default as Cart} from './cart'
 export {default as UserOrders} from './UserOrders'
